c65a2e0f9e3e6a87988b72ce86855ad01d6545e2
free-flow-code <wildmuskaria8@gmail.com>
2023-03-05 23:20:24 +0400

    Renamed variables to improve code readability

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ load_dotenv()
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Сокращение ссылок с помощью API Bitly')
-    parser.add_argument ('name', help='Вставьте одну или несколько ссылок', nargs='+', default=[''])
+    parser.add_argument('name', help='Вставьте одну или несколько ссылок', nargs='+', default=[''])
     return parser
 
 
 def shorten_link(url, BITLY_TOKEN):
     site = 'https://api-ssl.bitly.com/v4/bitlinks'
     header = {'Authorization': f'Bearer {BITLY_TOKEN}'}
-    data = {'long_url': url}
-    response = requests.post(site, headers=header, json=data)
+    long_url = {'long_url': url}
+    response = requests.post(site, headers=header, json=long_url)
     response.raise_for_status()
     items = response.json()
     return items['link']
 
 
 def strip_scheme(url):
-    parsed = urlparse(url)
-    bit_url = ''.join([parsed.netloc, parsed.path])
+    parse_url = urlparse(url)
+    bit_url = ''.join([parse_url.netloc, parse_url.path])
     return bit_url
 
 
@@ -51,8 +51,8 @@ def is_bitlink(url, BITLY_TOKEN):
 def main():
     BITLY_TOKEN = os.environ['BITLY_TOKEN']
     parser = create_parser()
-    namespace = parser.parse_args(sys.argv[1:])
-    for url in namespace.name:
+    urls = parser.parse_args(sys.argv[1:])
+    for url in urls.name:
         try:
             if is_bitlink(url, BITLY_TOKEN):
                 count = count_clicks(url, BITLY_TOKEN)
