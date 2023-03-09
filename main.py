5af7594a324fa1ae0a8a8c5f0daa0da4a146acf3
free-flow-code <wildmuskaria8@gmail.com>
2023-03-09 22:11:58 +0400

    renamed variables and functions to improve code readability

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ from dotenv import load_dotenv
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Сокращение ссылок с помощью API Bitly')
-    parser.add_argument('name', help='Вставьте одну или несколько ссылок', nargs='+', default=[''])
+    parser.add_argument('user_urls', help='Вставьте одну или несколько ссылок', nargs='+', default=[''])
     return parser
 
 
-def shorten_link(url, BITLY_TOKEN):
+def shorten_link(url, bitly_token):
     site = 'https://api-ssl.bitly.com/v4/bitlinks'
-    header = {'Authorization': f'Bearer {BITLY_TOKEN}'}
+    header = {'Authorization': f'Bearer {bitly_token}'}
     long_url = {'long_url': url}
     response = requests.post(site, headers=header, json=long_url)
     response.raise_for_status()
-    items = response.json()
-    return items['link']
+    bitlink = response.json()
+    return bitlink['link']
 
 
-def strip_scheme(url):
-    parse_url = urlparse(url)
-    bit_url = ''.join([parse_url.netloc, parse_url.path])
+def create_biturl(url):
+    parsed_url = urlparse(url)
+    bit_url = ''.join([parsed_url.netloc, parsed_url.path])
     return bit_url
 
 
-def count_clicks(url, BITLY_TOKEN):
-    bit_url = strip_scheme(url)
+def count_clicks(url, bitly_token):
+    bit_url = create_biturl(url)
     site =f'https://api-ssl.bitly.com/v4/bitlinks/{bit_url}/clicks/summary'
-    header = {'Authorization': f'Bearer {BITLY_TOKEN}'}
+    header = {'Authorization': f'Bearer {bitly_token}'}
     params = {'unit': 'day', 'units': '-1'}
     response = requests.get(site, headers=header, params=params)
     response.raise_for_status()
@@ -40,25 +40,25 @@ def count_clicks(url, BITLY_TOKEN):
     return counts['total_clicks']
 
 
-def is_bitlink(url, BITLY_TOKEN):
-    bit_url = strip_scheme(url)
+def is_bitlink(url, bitly_token):
+    bit_url = create_biturl(url)
     site = f'https://api-ssl.bitly.com/v4/bitlinks/{bit_url}'
-    header = {'Authorization': f'Bearer {BITLY_TOKEN}'}
+    header = {'Authorization': f'Bearer {bitly_token}'}
     response = requests.get(site, headers=header)
     return response.ok
 
 
 def main():
-    BITLY_TOKEN = os.environ['BITLY_TOKEN']
+    bitly_token = os.environ['BITLY_TOKEN']
     parser = create_parser()
-    urls = parser.parse_args(sys.argv[1:])
-    for url in urls.name:
+    urls = parser.parse_args()
+    for url in urls.user_urls:
         try:
-            if is_bitlink(url, BITLY_TOKEN):
-                count = count_clicks(url, BITLY_TOKEN)
+            if is_bitlink(url, bitly_token):
+                count = count_clicks(url, bitly_token)
                 print('Количество переходов по ссылке bitly: ', count)
             else:
-                bitlink = shorten_link(url, BITLY_TOKEN)
+                bitlink = shorten_link(url, bitly_token)
                 print('Битлинк: ', bitlink)
         except requests.exceptions.HTTPError as exc:
             print(exc)
